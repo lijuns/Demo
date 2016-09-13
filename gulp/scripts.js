7d07e292c4e46d2dfa07cdb5758541c44e872281
Jin Xu (External Consultant) <ext.jin.xu@EF.com>
2016-09-13 11:48:25 +0800

    concat imported to control the order of the scripts

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -4,6 +4,10 @@ var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
 
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'uglify-save-license', 'del']
+});
+
 var browserSync = require('browser-sync');
 
 var $ = require('gulp-load-plugins')();
@@ -12,6 +16,9 @@ gulp.task('scripts', ['scripts-bower'], function() {
     return gulp.src(
             path.join(conf.paths.src, '/js/app.js')
         )
+        .pipe($.sourcemaps.init())
+        .pipe($.concat('app.js'))
+        .pipe($.sourcemaps.write('maps'))
         .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/scripts/')))
         .pipe(browserSync.reload({ stream: true }))
         .pipe($.size())
@@ -20,8 +27,12 @@ gulp.task('scripts', ['scripts-bower'], function() {
 gulp.task('scripts-bower', function() {
     return gulp.src([
     		// put out-sourced (like bower) scripts here
-    		path.join(conf.paths.bower, '/jquery/dist/jquery.min.js')
+            path.join(conf.paths.bower, '/jquery/dist/jquery.min.js'),
+    		path.join(conf.paths.bower, '/bootstrap-sass/assets/javascripts/bootstrap.min.js')
     	])
+        .pipe($.sourcemaps.init())
+        .pipe($.concat('libs.js'))
+        .pipe($.sourcemaps.write('maps'))
         .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/libs/')))
         .pipe(browserSync.reload({ stream: true }))
         .pipe($.size())
